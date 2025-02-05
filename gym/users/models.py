bb88ca8ebefaa5171f5fd8dcf283a38c7a27b109
AngelikiMt <matta.d.angeliki@gmail.com>
2025-02-05 20:51:50 +0200

    views, models

--- gym/users/models.py
+++ gym/users/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+BILLING_DURATION_CHOICES = (
+    ("MONTHLY", "Monthly"),
+    ("QUARTERLY", "Quarterly"),
+    ("HALF YEARLY", "Half Yearly"),
+    ("YEARLY", "Yearly"),
+)
+
+class User(models.Model):
+    firstName = models.CharField(max_length=20, help_text="Please, enter your first name")
+    lastName = models.CharField(max_length=20, help_text="Please, enter your last name")
+    username = models.CharField(unique=True, max_length=20, help_text="Please, enter your username")
+    password = models.CharField(max_length=20, help_text="Please, enter your password")
+    email = models.EmailField("Email", unique=True, max_length=100)
+    dob = models.DateField("Date of Birth", help_text="Please, enter your date of birth")
+    date_of_registration = models.DateTimeField("Date of Registration", auto_now=True)
+    subscription = models.ForeignKey('Subscription', on_delete=models.RESTRICT, null=True, blank=True)
+
+    is_active = models.BooleanField("Active", default=False)
+    is_staff = models.BooleanField("Staff", default=False)
+    is_paid = models.BooleanField("Subscription payment", default=False)
+    
+
+    class Meta:
+        ordering = ['-lastName', '-firstName']
+
+    def __str__(self):
+        return self.lastName
+    
+class Subscription(models.Model):
+    subscription_name = models.CharField("Subscription Name", max_length=20)
+    fees = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    billing_duration = models.CharField("Billing Duration", max_length=50, choices=BILLING_DURATION_CHOICES, null=True, blank=True)
+
+    class Meta:
+        ordering = ['-subscription_name', '-billing_duration']
+
+
+    def __str__(self):
+        return self.subscription_name
+
--- gym/users/views.py
+++ gym/users/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import User, Subscription
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required, permission_required
+from django.http import HttpResponseRedirect
+
+def index(request):
+    return render(request, 'index.html')
+
+
+class ListUsers(ListView):
+    """Listing all users"""
+
+    model = User
+    template_name = 'users/list_users.html'
+    context_object_name = 'user_list'
+    queryset = User.objects.all().order_by('-lastName')
+
+class DetailUser(DetailView):
+    """User's details"""
+
+    models = User
+    template_name = 'users/details_user.html'
+    context_object_name = 'user_details'
+    queryset = User.objects.all()
+
+
+class ListSubscriptions(ListView):
+    """Listing all subscriptions"""
+
+    model = Subscription
+    template_name = 'users/list_subscritions.html'
+    context_object_name = 'subscriptions_list'
+    queryset = Subscription.objects.all()
+
+class DetailsSubscription(DetailView):
+    """Details of subscriptions"""
+
+    model = Subscription
+    template_name = 'users/details_subscription.html'
+    context_object_name = 'details_subscription'
+
+@login_required
+@permission_required('users.can_mark_returned', raise_exception=True)
+def get_users_details(request, pk):
+    userDetails = get_object_or_404(User, pk=pk)
+    return render(request, 'users/detail_user.html')
